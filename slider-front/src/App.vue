784b05e04081b97e05cecfe77b01759f21664df0
HuiSeokJeong <tobysug123@gmail.com>
2024-03-25 16:46:23 +0900

    test

---
+++ slider-front/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <GetItemTestVue/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+</template>
+
+<script>
+import GetItemTestVue from './components/GetItemTest.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {};
+  },
+  components: {
+    // HelloWorld,
+    GetItemTestVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
